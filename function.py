1e3755350be5518e8f1cb194d69dce69e6f63d13
SteffiLiao <SteffiLiao@users.noreply.github.com>
2020-12-11 22:22:28 +0800

    計算清單時長
    playlist 計算時間

--- function.py
+++ function.py
@@ -118,3 +118,66 @@ result_df.columns = ['tags', 'channelId', 'channelTitle', 'categoryId', 'title',
 
 # sort it by like_dislike_ratio value
 result_df.sort_values(by=['like_dislike_ratio'], ascending=False)
+
+
+
+# 計算清單時長
+def playlist_time(pl_id):
+    hours_pattern = re.compile(r'(\d+)H')
+    minutes_pattern = re.compile(r'(\d+)M')
+    seconds_pattern = re.compile(r'(\d+)S')
+    total_seconds = 0
+
+    nextPageToken = None
+    while True:
+        pl_request = youtube.playlistItems().list(
+            part='contentDetails',
+            playlistId=pl_id,
+            maxResults=50,          # 一個清單裡有多個video，且總影片數目大過於五
+            pageToken=nextPageToken
+        )
+
+        pl_response = pl_request.execute()
+
+        vid_ids = []
+        for item in pl_response['items']:
+            vid_ids.append(item['contentDetails']['videoId'])
+
+        vid_request = youtube.videos().list(
+            part="contentDetails",
+            id=','.join(vid_ids)
+        )
+
+        vid_response = vid_request.execute()
+
+        for item in vid_response['items']:
+            duration = item['contentDetails']['duration']
+
+            hours = hours_pattern.search(duration)
+            minutes = minutes_pattern.search(duration)
+            seconds = seconds_pattern.search(duration)
+
+            hours = int(hours.group(1)) if hours else 0
+            minutes = int(minutes.group(1)) if minutes else 0
+            seconds = int(seconds.group(1)) if seconds else 0
+
+            video_seconds = timedelta(
+                hours=hours,
+                minutes=minutes,
+                seconds=seconds
+            ).total_seconds()
+
+            total_seconds += video_seconds
+
+        nextPageToken = pl_response.get('nextPageToken')
+
+        if not nextPageToken:
+            break
+
+    total_seconds = int(total_seconds)
+
+    minutes, seconds = divmod(total_seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+
+    print(f'{hours}:{minutes}:{seconds}')  # 62:9:43
+

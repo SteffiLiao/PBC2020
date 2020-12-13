d457071bf0c2dffaff9258f99b85a6c000406a1d
Cindy-Chiang-Hoshino <75737436+Cindy-Chiang-Hoshino@users.noreply.github.com>
2020-12-13 20:11:28 +0800

    輸入頻道ID獲得頻道簡介

---
+++ 輸入頻道ID獲得頻道簡介.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+from googleapiclient.discovery import build
+print("Please enter your channel ID")
+channel_id=input() #輸入頻道ID
+api_key = 'AIzaSyDCnkRjeXJLQmXK30A4YXnOWsi-7RwxTb4'
+youtube = build('youtube', 'v3', developerKey=api_key)
+res = youtube.channels().list(
+    id=channel_id,
+    part='brandingSettings').execute()
+description=res['items'][0]['brandingSettings']['channel']['description']
+print(description)

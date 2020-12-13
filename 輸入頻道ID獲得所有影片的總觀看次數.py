074af4cba544dd76ce3adf0994c209d3e476d588
Cindy-Chiang-Hoshino <75737436+Cindy-Chiang-Hoshino@users.noreply.github.com>
2020-12-13 18:12:51 +0800

    輸入頻道ID獲得所有影片的總觀看次數

---
+++ 輸入頻道ID獲得所有影片的總觀看次數.py
@@ -0,0 +1,13 @@
+import urllib.request
+
+import json
+
+print("Please enter your youtube channel ID")
+name=input()  #輸入頻道的id
+
+key='AIzaSyDCnkRjeXJLQmXK30A4YXnOWsi-7RwxTb4'
+
+
+data = urllib.request.urlopen("https://www.googleapis.com/youtube/v3/channels?part=statistics&id="+name+"&key="+key).read() #讀取資料
+total_view=json.loads(data)['items'][0]['statistics']['viewCount']
+print("You have %d" %int(total_view) +" total view")

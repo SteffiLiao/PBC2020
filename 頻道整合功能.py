5a1b3e0e629cc023ec3a7b7ba417c3a954e4cd74
Cindy-Chiang-Hoshino <75737436+Cindy-Chiang-Hoshino@users.noreply.github.com>
2020-12-16 00:06:28 +0800

    Add files via upload
    將總觀看次數、總訂閱人數、簡介寫成函式後整理成一份。

---
+++ 頻道整合功能.py
@@ -0,0 +1,42 @@
+from googleapiclient.discovery import build
+import urllib.request
+import json
+
+print("Please enter your youtube channel ID")
+name = input()  # 輸入頻道的id
+
+
+def total_view_time(name):  # 輸入頻道ID獲取該頻道的總觀看次數
+    key = 'AIzaSyDCnkRjeXJLQmXK30A4YXnOWsi-7RwxTb4'
+    data = urllib.request.urlopen(
+        "https://www.googleapis.com/youtube/v3/channels?part=statistics&id=" +
+        name +
+        "&key=" +
+        key).read()  # 讀取資料
+    total_view = json.loads(data)['items'][0]['statistics']['viewCount']
+    print("You have %d" % int(total_view) + "total view")
+
+def total_subscribers(name):  # 輸入頻道ID獲取該頻道的總訂閱數
+    key = 'AIzaSyDCnkRjeXJLQmXK30A4YXnOWsi-7RwxTb4'
+    data = urllib.request.urlopen(
+        "https://www.googleapis.com/youtube/v3/channels?part=statistics&id=" +
+        name +
+        "&key=" +
+        key).read()
+    subs = json.loads(data)["items"][0]["statistics"]["subscriberCount"]
+    print("You have %d" % int(subs) + " subscribers")
+
+
+def channel_description(name):  # 輸入頻道ID獲取頻道簡介
+    api_key = 'AIzaSyDCnkRjeXJLQmXK30A4YXnOWsi-7RwxTb4'
+    youtube = build('youtube', 'v3', developerKey=api_key)
+    res = youtube.channels().list(
+        id=name,
+        part='brandingSettings').execute()
+    description = res['items'][0]['brandingSettings']['channel']['description']
+    print(description)
+
+total_view_time(name)
+total_subscribers(name)
+channel_description(name)
+
